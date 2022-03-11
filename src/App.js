48e7c0d0aff77a73418f4671068c775fcb1244dd
Rıdvan Akca <akca.ridvann@gmail.com>
2022-03-11 16:39:53 +0300

    Implement github repo search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import RepoTable from "./components/RepoTable";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+
+  const handleSearchClick = async () => {
+    setLoading(true);
+
+    // fetch(`https://api.github.com/search/repositories?q=${query}`)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setRepos(data.items);
+    //   })
+    //   .catch(error => console.error(error))
+    //   .finally(() => {
+    //     setLoading(false);
+    //   }); 
+
+    try {
+      const response = await fetch(`https://api.github.com/search/repositories?q=${query}`);
+      const data = await response.json();
+      setRepos(data.items);
+    } catch(error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <TextField
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          id='outlined-basic'
+          label='Repository'
+          variant='outlined'
+        />
+        <Button
+          onClick={handleSearchClick}
+          className='app-btn'
+          variant='contained'>
+          Search
+        </Button>
+      </div>
+      {loading ? 'Loading' : <RepoTable repos={repos} />}
+    </>
   );
 }
 
---
+++ src/components/RepoTable.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+export default function RepoTable({ repos }) {
+  if (repos.length === 0) {
+    return null;
+  }
+  
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>Repository</TableCell>
+            <TableCell align='right'>Owner</TableCell>
+            <TableCell align='right'>Star</TableCell>
+            <TableCell align='right'>Forks Count</TableCell>
+            <TableCell align='right'>Created At</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {repos.map((repo, index) => (
+            <TableRow
+              key={index}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+              <TableCell component='th' scope='row'>
+                {repo.name}
+              </TableCell>
+              <TableCell align='right'>{repo.owner.login}</TableCell>
+              <TableCell align='right'>{repo.stargazers_count}</TableCell>
+              <TableCell align='right'>{repo.forks_count}</TableCell>
+              <TableCell align='right'>{repo.created_at}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
